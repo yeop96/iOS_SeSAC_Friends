1547707999779459515c621712fae4f1d9cb4b3e
yeop96 <ekdh6@naver.com>
2022-02-12 00:26:08 +0900

    Home main 기능 구현

--- SeSAC_Friends/ViewControllers/Home/HomeViewController.swift
+++ SeSAC_Friends/ViewControllers/Home/HomeViewController.swift
@@ -11,7 +11,6 @@ import MapKit
 import CoreLocation
 import JGProgressHUD
 
-
 class HomeViewController: BaseViewController {
     let mapView = MKMapView()
     let progress = JGProgressHUD()
@@ -21,15 +20,22 @@ class HomeViewController: BaseViewController {
     let filterButton = FilterButton()
     let gpsButton = GPSButton()
     var floatingButton = FloatingButton(status: .search)
+    let centerLocationView = UIImageView()
     var searchedFriends: SearchedFriends?
     var pickLocation: PickLocation?
     var currentLocation: CLLocation!
     var maleAnnotations = [CustomAnnotation]()
     var femaleAnnotations = [CustomAnnotation]()
+    var selectFilter = GenderNumber.unSelect.rawValue
+    var lat = 0.0
+    var long = 0.0
+    var region = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.hideNavigationBar()
+        print("아이디토큰", UserData.idToken)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,6 +45,12 @@ class HomeViewController: BaseViewController {
     
     override func configure() {
         navigationController?.changeNavigationBar(isClear: true)
+        centerLocationView.image = UIImage(named: "map_marker")
+      
+        filterButton.allButton.addTarget(self, action: #selector(filterAllButtonClicked), for: .touchUpInside)
+        filterButton.allButtonClicked(sender: filterButton.allButton)
+        filterButton.maleButton.addTarget(self, action: #selector(filterMaleButtonClicked), for: .touchUpInside)
+        filterButton.femaleButton.addTarget(self, action: #selector(filterFemaleButtonClicked), for: .touchUpInside)
         
         mapView.delegate = self
         mapView.setRegion(MKCoordinateRegion(center: defaultCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
@@ -48,14 +60,9 @@ class HomeViewController: BaseViewController {
         locationManager.startMonitoringSignificantLocationChanges()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         currentLocation = locationManager.location
+        mapView.register(CustomAnnotationView.self, forAnnotationViewWithReuseIdentifier: CustomAnnotationView.identifier)
         
-        
-        pin.coordinate = defaultCoordinate
-        pin.title = "기본 위치"
-        pin.subtitle = "위치 권한 설정을 허용해주세요"
-        mapView.addAnnotation(pin)
 
-        
         gpsButton.addTarget(self, action: #selector(findMyLocation), for: .touchUpInside)
     }
     
@@ -64,6 +71,7 @@ class HomeViewController: BaseViewController {
         view.addSubview(filterButton)
         view.addSubview(gpsButton)
         view.addSubview(floatingButton)
+        view.addSubview(centerLocationView)
         mapView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
@@ -78,6 +86,37 @@ class HomeViewController: BaseViewController {
         floatingButton.snp.makeConstraints { make in
             make.trailing.equalToSuperview().inset(16)
             make.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
+        centerLocationView.snp.makeConstraints { make in
+            make.centerX.equalTo(mapView)
+            make.centerY.equalTo(mapView).offset(-24)
+            make.size.equalTo(48)
+        }
+    }
+    
+    @objc func filterAllButtonClicked(){
+        selectFilter = GenderNumber.unSelect.rawValue
+        searchFreinds()
+    }
+    @objc func filterMaleButtonClicked(){
+        selectFilter = GenderNumber.male.rawValue
+        searchFreinds()
+    }
+    @objc func filterFemaleButtonClicked(){
+        selectFilter = GenderNumber.female.rawValue
+        searchFreinds()
+    }
+    
+    func addFilteredPin(gender: Int){
+        mapView.removeAnnotations(self.mapView.annotations)
+        switch gender {
+        case GenderNumber.male.rawValue:
+            mapView.addAnnotations(maleAnnotations)
+        case GenderNumber.female.rawValue:
+            mapView.addAnnotations(femaleAnnotations)
+        default:
+            mapView.addAnnotations(maleAnnotations)
+            mapView.addAnnotations(femaleAnnotations)
         }
     }
     
@@ -132,11 +171,36 @@ class HomeViewController: BaseViewController {
     func searchFreinds(){
         progress.show(in: view, animated: true)
         DispatchQueue.global().async {
-            ServerService.shared.postSearchFriedns(region: 1275130688, lat: 37.517819364682694, long: 126.88647317074734) { statusCode, data in
+            ServerService.shared.postSearchFriedns(region: self.region, lat: self.lat, long: self.long) { statusCode, data in
                 switch statusCode{
                 case ServerStatusCode.OK.rawValue:
                     DispatchQueue.main.async {
+                        self.maleAnnotations = []
+                        self.femaleAnnotations = []
+                        
                         self.searchedFriends = try? JSONDecoder().decode(SearchedFriends.self, from: data!)
+                        
+                        for otherUserInfo in self.searchedFriends!.fromQueueDB {
+                            
+                            if otherUserInfo.gender == GenderNumber.male.rawValue {
+                                self.maleAnnotations.append(CustomAnnotation(sesac_image: otherUserInfo.sesac, coordinate: CLLocationCoordinate2D(latitude: otherUserInfo.lat, longitude: otherUserInfo.long)))
+                            } else {
+                                self.femaleAnnotations.append(CustomAnnotation(sesac_image: otherUserInfo.sesac, coordinate: CLLocationCoordinate2D(latitude: otherUserInfo.lat, longitude: otherUserInfo.long)))
+                            }
+                        }
+                        
+                        for otherUserInfo in self.searchedFriends!.fromQueueDBRequested {
+                            
+                            if otherUserInfo.gender == GenderNumber.male.rawValue {
+                                self.maleAnnotations.append(CustomAnnotation(sesac_image: otherUserInfo.sesac, coordinate: CLLocationCoordinate2D(latitude: otherUserInfo.lat, longitude: otherUserInfo.long)))
+                            } else {
+                                self.femaleAnnotations.append(CustomAnnotation(sesac_image: otherUserInfo.sesac, coordinate: CLLocationCoordinate2D(latitude: otherUserInfo.lat, longitude: otherUserInfo.long)))
+                            }
+                            
+                        }
+                        
+                        
+                        self.addFilteredPin(gender: self.selectFilter)
                         print("누구인가", self.searchedFriends as Any)
                     }
                 case ServerStatusCode.FIREBASE_TOKEN_ERROR.rawValue:
@@ -153,11 +217,8 @@ class HomeViewController: BaseViewController {
                     print("ERROR: ", statusCode)
                 }
             }
-            
             self.progress.dismiss(animated: true)
         }
-        
-        
     }
 
     
@@ -166,38 +227,64 @@ class HomeViewController: BaseViewController {
 
 extension HomeViewController: MKMapViewDelegate, CLLocationManagerDelegate{
     
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         locationManager = manager
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             currentLocation = locationManager.location
         }
+        
+//        guard let location = locations.last else { return }
+//
+//        lat = location.coordinate.latitude
+//        long = location.coordinate.longitude
+//        let center = CLLocationCoordinate2D(latitude: lat, longitude: long)
+//
+//        let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+//
+//
+//        mapView.setRegion(region, animated: true)
     }
     
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
-        guard !annotation.isKind(of: MKUserLocation.self) else {
-            let annotationView = MKAnnotationView()
-            annotationView.annotation = annotation
-            annotationView.image = UIImage(named: "map_marker")
-            return annotationView
+        guard let annotation = annotation as? CustomAnnotation else {
+            return nil
         }
         
-        var annotationView = self.mapView.dequeueReusableAnnotationView(withIdentifier: "Custom")
+        var annotationView = self.mapView.dequeueReusableAnnotationView(withIdentifier: CustomAnnotationView.identifier)
         
         if annotationView == nil {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "Custom")
-            annotationView?.canShowCallout = true
-            let miniButton = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-            miniButton.setImage(UIImage(systemName: "person"), for: .normal)
-            miniButton.tintColor = .blue
-            annotationView?.rightCalloutAccessoryView = miniButton
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: CustomAnnotationView.identifier)
+            annotationView?.canShowCallout = false
+            annotationView?.contentMode = .scaleAspectFit
+            
         } else {
             annotationView?.annotation = annotation
         }
         
-        annotationView?.image = UIImage(named: "map_marker")
+        let sesacImage: UIImage!
+        let size = CGSize(width: 85, height: 85)
+        UIGraphicsBeginImageContext(size)
+        
+        sesacImage = SesacImage(rawValue: annotation.sesac_image ?? 0)?.sesacUIImage()
+    
+        sesacImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        annotationView?.image = resizedImage
+        
         return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        lat = mapView.centerCoordinate.latitude
+        long = mapView.centerCoordinate.longitude
+        
+        let center = CLLocation(latitude: lat, longitude: long)
+        region = controlRegion(lat: lat, long: long)
+        
+        searchFreinds()
     }
     
 }
@@ -217,4 +304,21 @@ class CustomAnnotation: NSObject, MKAnnotation {
     super.init()
   }
 
+}
+
+class CustomAnnotationView: MKAnnotationView {
+    
+    static let identifier = "CustomAnnotationView"
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?){
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        frame = CGRect(x: 0, y: 0, width: 40, height: 50)
+        centerOffset = CGPoint(x: 0, y: -frame.size.height / 2)
+        backgroundColor = .clear
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
